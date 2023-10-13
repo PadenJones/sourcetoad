6c4775ce8315f1d60bfd6dcb04039d465a06da3e
Paden Jones <padenjones@outlook.com>
2023-10-12 20:12:45 -0400

    [PJ] ran linter on ProblemOne

--- ProblemOne/index.js
+++ ProblemOne/index.js
@@ -4,8 +4,8 @@ var arr = [
     'first_name': 'Marco',
     'last_name': 'Burns',
     'guest_booking': {
-        'room_no': 'A0073',
-        'some_array': [7,2,4]
+      'room_no': 'A0073',
+      'some_array': [7, 2, 4]
     },
   },
   {
@@ -13,8 +13,8 @@ var arr = [
     'first_name': 'John',
     'last_name': 'Doe',
     'guest_booking': {
-        'room_no': 'C73',
-        'some_array': [1,3,5,2,4,3]
+      'room_no': 'C73',
+      'some_array': [1, 3, 5, 2, 4, 3]
     },
   },
   {
@@ -22,8 +22,8 @@ var arr = [
     'first_name': 'Jane',
     'last_name': 'Doe',
     'guest_booking': {
-        'room_no': 'C73',
-        'some_array': [1,3,5,2,4,3]
+      'room_no': 'C73',
+      'some_array': [1, 3, 5, 2, 4, 3]
     },
   },
   {
@@ -31,8 +31,8 @@ var arr = [
     'first_name': 'Albert',
     'last_name': 'Einstein',
     'guest_booking': {
-        'room_no': 'B15',
-        'some_array': [2,5,6,3]
+      'room_no': 'B15',
+      'some_array': [2, 5, 6, 3]
     },
   },
   {
@@ -40,8 +40,8 @@ var arr = [
     'first_name': 'Jack',
     'last_name': 'Daniels',
     'guest_booking': {
-        'room_no': 'B15',
-        'some_array': [2,5,6,3]
+      'room_no': 'B15',
+      'some_array': [2, 5, 6, 3]
     },
   },
   {
@@ -49,17 +49,17 @@ var arr = [
     'first_name': 'Alan',
     'last_name': 'Turing',
     'guest_booking': {
-        'room_no': 'B15',
-        'some_array': [2,5,6,3]
+      'room_no': 'B15',
+      'some_array': [2, 5, 6, 3]
     },
   },
 ];
 
 function mutateArray(a) {
-    return a;
+  return a;
 }
 
-$(document).ready(function() {
-    $('#originalArray').html(JSON.stringify(arr, null, 2));
-    $('#resultsArray').html(JSON.stringify(mutateArray(arr), null, 2));
+$(document).ready(function () {
+  $('#originalArray').html(JSON.stringify(arr, null, 2));
+  $('#resultsArray').html(JSON.stringify(mutateArray(arr), null, 2));
 });
